ecfecc1b16fec41d73037ff83d82f44d8c11cf70
Kartik Pant <kartikpant.kp69@gmail.com>
2025-07-24 14:01:25 +0530

    Update Peak_mountain_element.cpp

--- Binary_Search/LEETCODE/Peak_mountain_element.cpp
+++ Binary_Search/LEETCODE/Peak_mountain_element.cpp
@@ -37,14 +37,14 @@
 
 //  logic 
 
-int start=0,end=arr.size()-1;
-int mid = (start+end)/2;
-while(start<end){
-    if(arr[mid]<arr[mid+1]){
-        start=mid+1;
-    }
-    else{
-        end =mid;
-    }
-    mid = (start+end)/2;
-} return start;
+// int start=0,end=arr.size()-1;
+// int mid = (start+end)/2;
+// while(start<end){
+//     if(arr[mid]<arr[mid+1]){
+//         start=mid+1;
+//     }
+//     else{
+//         end =mid;
+//     }
+//     mid = (start+end)/2;
+// } return start;
